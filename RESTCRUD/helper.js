9f0cb581574dece33ebcfbde2666af303e196bb9
Nathalie <nathaliemekahal@gmail.com>
2020-05-28 12:04:10 +0200

    g

--- RESTCRUD/helper.js
+++ RESTCRUD/helper.js
@@ -4,6 +4,11 @@ const getEvents=async()=>{
     let response = await fetch(url)//this is getting response from api fetching 
     return await response.json()//this is returning the result of thr promise in a usable format
 }
+const getEvent=async(id)=>{
+    console.log(url + id)
+    let response = await fetch(url + id) //this is getting response from api fetching 
+    return await response.json()//this is returning the result of thr promise in a usable format
+}
 const saveEvent =async (agendaEvent)=>{
     let response= await fetch(url,{
         method:"POST",
